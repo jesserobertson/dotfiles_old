78f3ed5dadf7e18dafcbe70dac763823dc7d7f10
Jess Robertson <jesse.robertson@csiro.au>
2017-06-30 09:07:05 +0800

    Break out conda install for easier access

--- setup.sh
+++ setup.sh
@@ -34,27 +34,6 @@ git clone https://github.com/rking/ag.vim bundle/ag
 wget -qO- https://get.docker.com/ | sh
 
 # Add current user to docker group for non-root access
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker $USER
 
-## INSTALL CONDA
-# Set up where we're going to stick it
-export CONDA_URL="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-export CONDA_HOME="${LOCAL}/conda"
-
-# Set up paths
-# Note that conda spews if CONDA_HOME already exists
-mkdir -p ${LOCAL}/bin
-
-# Actually do install
-rm -rf ${CONDA_HOME}
-wget --quiet ${CONDA_URL} -O miniconda.sh
-/bin/bash miniconda.sh -b -p ${CONDA_HOME}
-rm miniconda.sh
-${CONDA_HOME}/bin/conda install --yes conda==3.10.1
-export PATH=${CONDA_HOME}/bin:$PATH
-
-# Configure conda to use Jess' binstar channel
-conda config --set always_yes yes --set changeps1 no
-conda config --add channels jesserobertson
-conda update -q conda
-conda info
+bash install_conda.sh
