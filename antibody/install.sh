144198f759f80f37bcf6654897a302a22125c950
Jess Robertson <jesse.robertson@mbie.govt.nz>
2020-07-22 01:21:47 +0000

    Make antibody install locally

--- antibody/install.sh
+++ antibody/install.sh
@@ -3,7 +3,8 @@ if command -v brew >/dev/null 2>&1; then
 	brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
 	brew install antibody
 else
-	curl -sL https://git.io/antibody | sudo sh -s -- -b /usr/local/bin
+	mkdir -p ~/.local/bin
+	curl -sL https://git.io/antibody | sh -s -- -b ~/.local/bin
 fi
 antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
 antibody update
